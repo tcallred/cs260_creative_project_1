cdc0e77d9044f8c41b68cc176670101ba9a76f76
tcallred <taylor.allred21@gmail.com>
2017-09-20 21:16:06 +0000

    Added some real quotes to JS file

--- script.js
+++ script.js
@@ -3,23 +3,18 @@ var quotes;
 var response;
 var index = -1;
 window.onload = function() {
-	quotes = ["a","b","c"];
-	quotes.push("d");
-	quotes.push("e");
-	quotes.push("f");
-	quotes.push("g");
-	quotes.push("h");
-	quotes.push("i");
-	quotes.push("j");
-	quotes.push("k");
+	quotes = ["Do not take life too seriously. You will never get out of it alive.",
+	"A day without sunshine is like, you know, night.",
+	"People who think they know everything are a great annoyance to those of us who do.",
+	"Procrastination is the art of keeping up with yesterday."];
 
-}		
+}
 function get_quote() {
 	var new_index = index;
 	while(new_index == index) {
-		new_index = Math.floor(Math.random() * Math.floor(quotes.length));		
+		new_index = Math.floor(Math.random() * Math.floor(quotes.length));
 	}
 	index = new_index;
 	response = "<p>" + quotes[index] + "</p>";
 	document.getElementById("quote").innerHTML = response;
-}		
+}
